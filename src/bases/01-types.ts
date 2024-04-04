fb64d3e1a109241d8301ee0e1a16d40d57f0c81f
drmiltonc <92145221+drmiltonc@users.noreply.github.com>
2024-04-04 00:54:01 -0500

    first commit

---
+++ src/bases/01-types.ts
@@ -0,0 +1,5 @@
+
+export const name: string = 'Milton';
+export const age: number = 40;
+
+console.log(name);
---
+++ src/bases/02-objects.ts
@@ -0,0 +1,9 @@
+
+export const pokemonIds = [1, 2, 3, 4, 5];
+
+export const pokemon= {
+    id: 1,
+    name: 'Pikachu',
+    
+}
+
