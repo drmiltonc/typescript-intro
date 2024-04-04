4ebbcf03c81430875d8325aac4b7e018488eabf0
drmiltonc <92145221+drmiltonc@users.noreply.github.com>
2024-04-04 02:19:48 -0500

    http

--- src/bases/03-clases.ts
+++ src/bases/03-clases.ts
@@ -1,3 +1,5 @@
+import axios from "axios";
+import { Move, PokeAPIResponse } from "../interfaces/poke-api.interface";
 
 
 export class Pokemon {
@@ -15,6 +17,15 @@ export class Pokemon {
         console.log(`${bulbasur.name}, ${bulbasur.name}`);
         this.speak();
     }
+    
+    async getMoves(): Promise<Move[]> {
+        //const moves = 10;
+
+        const {data} = await axios.get<PokeAPIResponse>('https://pokeapi.co/api/v2/pokemon/bulbasaur');
+        console.log(data.moves);
+        
+        return data.moves;
+    }
 
     constructor(public readonly id: number, public name: string,) {
     }
@@ -26,4 +37,6 @@ export const bulbasur = new Pokemon(
     'Bulbasur',
 
 );
+
+console.log(bulbasur.getMoves());
 
